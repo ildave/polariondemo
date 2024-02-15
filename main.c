3d0546490633a04e5b25459be231c8064b26c137
Davide Gualano <ildave@gmail.com>
2024-02-15 13:02:06 +0100

    ST-394 Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 
+/**
+* @wi.implements STRT/ST-394 Returns sum
+*/
+int add(int a, int b) {
+  return a + b;
+}
+
+
 int main(int argc, char *argv[]) {
   int a = 0;
   int b = 0;
